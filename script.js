1cbe0222e46c4fdde199befa2c909788b14e3521
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-11-12 22:50:38 -0500

    title

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.navOne').click(function(){
+  window.open('./products.html', '_self');
+  return false;
+});
+
+$('.navTwo').click(function(){
+  window.open('./reviews.html', '_self');
+  return false;
+});
+
+$('.navThree').click(function(){
+  window.open('./pricing.html', '_self');
+  return false;
+});
+
+$('.navFour').click(function(){
+  window.open('./contact.html', '_self');
+  return false;
+});
+
+$('.navFive').click(function(){
+  window.open('./about.html', '_self');
+  return false;
+});
