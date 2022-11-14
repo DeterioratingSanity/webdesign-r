ffea568e730614b536858432a82c8676bc4d3db8
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-11-14 00:25:11 -0500

    nav bar / each; home button thereto

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ $('.navTwo').click(function(){
 });
 
 $('.navThree').click(function(){
-  window.open('./pricing.html', '_self');
-  return false;
-});
-
-$('.navFour').click(function(){
   window.open('./contact.html', '_self');
   return false;
 });
@@ -22,3 +17,7 @@ $('.navFive').click(function(){
   window.open('./about.html', '_self');
   return false;
 });
+$('.navH').click(function(){
+  window.open('./index.html', '_self');
+  return false;
+})
